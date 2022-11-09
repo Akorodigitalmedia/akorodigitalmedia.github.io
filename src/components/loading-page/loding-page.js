706f46a06f58057ab58c7a1845fee2b67fefd00f
Whitney de Oliveira Okafor <114311806+WhitneyOOkafor@users.noreply.github.com>
2022-11-09 13:42:10 +0200

    changed instagram link. All links work now

--- src/components/loading-page/loding-page.js
+++ src/components/loading-page/loding-page.js
@@ -12,7 +12,7 @@ class loadingPage extends React.Component{
                 {/* TODO add real social media icons */}
                 <SocialMediaIcon img_src="././assets/behance.svg" link_destination="https://www.behance.net/AkoroDigitalMedia"/>
                 <SocialMediaIcon img_src="././assets/facebook.svg" link_destination="https://www.facebook.com/akorodigitalmedia"/>
-                <SocialMediaIcon img_src="././assets/instagram.svg" link_destination="http://instagram.com"/>
+                <SocialMediaIcon img_src="././assets/instagram.svg" link_destination="http://instagram.com/akorodigitalmedia"/>
                 <SocialMediaIcon img_src="././assets/likedin.svg" link_destination="https://www.linkedin.com/company/akoro-digital-media/"/>
                 <SocialMediaIcon img_src="././assets/youtube.svg" link_destination="https://www.youtube.com/channel/UCzef-cWtG-A3MSoVzIXxq2Q"/>
             </div>
