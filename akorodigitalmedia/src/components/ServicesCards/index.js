87d46e59d63950ab7ea498e7c3e0ca2558cd1c5d
Whitney de Oliveira Okafor <114311806+WhitneyOOkafor@users.noreply.github.com>
2023-02-01 10:44:10 +0200

    created the services card

---
+++ akorodigitalmedia/src/components/ServicesCards/index.js
@@ -0,0 +1,22 @@
+import style from "./ServicesCards.module.css";
+import Image from "next/image";
+
+export default function CardBody(props) {
+  return (
+    <div className={style.CardContainer}>
+      <div className={style.IconOuter}>
+        <div className={style.IconInner}>
+          <Image
+            src={props.ImageSource}
+            alt={props.AltText}
+            style={{ height: "8.33vw", width: "auto" }}
+            className={style.Icon}
+          />
+        </div>
+      </div>
+      <div className={style.CardTitle}>
+        <h6>{props.Title}</h6>
+      </div>
+    </div>
+  );
+}
--- akorodigitalmedia/src/sections/ServicesSectionTwo/index.js
+++ akorodigitalmedia/src/sections/ServicesSectionTwo/index.js
@@ -1,9 +1,22 @@
 import style from "./ServicesSectionTwo.module.css";
+import Card from "../../components/ServicesCards";
+import Image from "next/image";
+
+//Card Icons
+import Modelling from "../../../public/assets/3d-modelling-icon.png";
+import Animation from "../../../public/assets/animation-icon.png";
+import BrandDesign from "../../../public/assets/brand-design-icon.png";
+import Illustration from "../../../public/assets/illustration-icon.png";
+import Photography from "../../../public/assets/photography-icon.png";
+import PrintDesign from "../../../public/assets/print-design-icon.png";
+import WebDesign from "../../../public/assets/web-design-icon.png";
+import WebDevelopment from "../../../public/assets/web-development-icon.png";
+
 export default function ServicesSection() {
   return (
     <div className={style.container}>
       <h2 className={style.Title}>Our Services</h2>
-      <p className="subtitle-1">
+      <p className={style.subtitle + " subtitle-1"}>
         We offer a range of services to cater to all your digital media design
         and development needs
       </p>
